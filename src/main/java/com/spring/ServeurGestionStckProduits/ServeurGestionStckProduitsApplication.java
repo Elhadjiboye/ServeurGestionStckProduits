c9b7357405459c104ae3e33e01a0eff5aa4063d8
Boye <boyeelhadjiabdou@gmail.com>
2024-01-07 23:23:06 +0000

    first commit

---
+++ src/main/java/com/spring/ServeurGestionStckProduits/ServeurGestionStckProduitsApplication.java
@@ -0,0 +1,13 @@
+package com.spring.ServeurGestionStckProduits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServeurGestionStckProduitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServeurGestionStckProduitsApplication.class, args);
+	}
+
+}
